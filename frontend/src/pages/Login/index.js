531d9310a162273bda90ac226483270a65805930
Jandrade2k <dev.jandrade@gmail.com>
2020-11-05 18:52:56 -0300

    tela de login feita

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './style.css';
+import Logo from './../../assets/flairs logo.png';
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="corpo">
+                <div className="card">
+                    <img src={Logo} />
+                    <div className="textos">
+                        <input placeholder="E-mail"></input>
+                        <input placeholder="Senha"></input>
+                    </div>
+                    <div className="btns">
+                        <button className="btn-sub" type="submit">Enviar</button>
+                        <button className="btn-reg" type="button">Pedir Registro</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
